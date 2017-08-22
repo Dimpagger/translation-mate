c464ecabe51c179a118d59937c93c698c8bb567f
阿宾 <shaniaojoker@163.com>
2017-08-23 02:00:33 +0800

    [dev] Initial commit.

---
+++ spec/translation-mate-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TranslationMateView from '../lib/translation-mate-view';
+
+describe('TranslationMateView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
